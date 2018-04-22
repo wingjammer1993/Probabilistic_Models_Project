8458544ef1c3be800786756118b58154134cdff6
amruta <hail.radical@gmail.com>
2018-04-21 21:47:13 -0600

    added noise

--- Part_1a.py
+++ Part_1a.py
@@ -11,14 +11,10 @@ def target(x, y):
     pos = np.empty(x.shape + (2,))
     pos[:, :, 0] = x
     pos[:, :, 1] = y
-    x_mean_1, x_mean_2 = np.random.choice(x[0, :], 2, replace=False)
-    y_mean_1, y_mean_2 = np.random.choice(y[:, 0], 2, replace=False)
-    mu_1 = np.array([x_mean_1, y_mean_1])
-    mu_2 = np.array([x_mean_2, y_mean_2])
     sigma_1 = np.array([[1., -0.5], [-0.5, 1.5]])
     sigma_2 = np.array([[1., 0.5], [0.5, 1.5]])
-    norm_1 = multivariate_normal(mu_1, sigma_1)
-    norm_2 = multivariate_normal(mu_2, sigma_2)
+    norm_1 = multivariate_normal([8, 0], sigma_1)
+    norm_2 = multivariate_normal([2, 2], sigma_2)
     z_1 = norm_1.pdf(pos)
     z_2 = norm_2.pdf(pos)
     z = np.add(z_1, z_2)
@@ -29,8 +25,8 @@ def target(x, y):
 
 
 if __name__ == "__main__":
-    fig = plt.figure()
-    ax = Axes3D(fig)
+    #fig = plt.figure()
+    #ax = Axes3D(fig)
     x_input = np.linspace(0, 10, 1000)
     y_input = np.linspace(-5, 5, 1000)
     x_mesh, y_mesh = np.meshgrid(x_input, y_input)
