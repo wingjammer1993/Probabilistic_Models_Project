20c4cc3687631fae3fb623facf5ee7d2ab69f91f
amruta <hail.radical@gmail.com>
2018-04-22 23:00:09 -0600

    added noise

---
+++ Part_2b.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_lds(alpha_1, alpha_2, sigma, samples):
+	mu_y1 = np.random.normal(0, sigma)
+	mu_y2 = np.random.normal(0, sigma)
+	mu_z = np.random.normal(0, sigma)
+	y_1 = [mu_y1]*samples
+	y_2 = [mu_y2]*samples
+	z = [alpha_1[0]*y_1[0] + alpha_2[0]*y_2[0] + mu_z]*samples
+	alpha_mode_1 = alpha_1[0]
+	alpha_mode_2 = alpha_2[0]
+	for i in range(1, samples):
+		if np.random.rand() < 0.3:
+			draw_mode = np.random.multinomial(1, [1 / 3] * 3, size=1)
+			alpha_mode_1 = alpha_1[np.argmax(draw_mode)]
+			alpha_mode_2 = alpha_2[np.argmax(draw_mode)]
+		y_2[i] = y_1[i-1] + mu_y2
+		y_1[i] = alpha_mode_1*y_1[i-1] + alpha_mode_2*y_2[i-1] + mu_y1
+		z[i] = y_1[i] + mu_z
+	plt.plot(z)
+	plt.xlabel("Time")
+	plt.ylabel("Observation")
+	plt.title("Switched Linear Dynamic System with 3 modes")
+	plt.show()
+
+
+generate_lds([1, 0.7, -0.75], [-0.5, -0.2, 0.3], 2, 200)
+
