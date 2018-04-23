c66f79cde771d1ca5544ff8fdd09d7bb63dbf040
amruta <hail.radical@gmail.com>
2018-04-23 14:57:26 -0600

    added noise

--- Part_2c.py
+++ Part_2c.py
@@ -1,7 +1,7 @@
 import numpy as np
 import copy
 import Part_2a
-
+import matplotlib.pyplot as plt
 
 class KalmanFilter:
 	def __init__(self, A, P, Q, H, R,):
@@ -22,8 +22,7 @@ class KalmanFilter:
 		kalman_gain = np.dot(p_pri, np.dot(self.matrix_H.T, np.linalg.inv(innovation_cov)))
 		x_post = x_pri + np.dot(kalman_gain, innovation)
 		term = np.dot(kalman_gain, self.matrix_H)
-		k = np.size(term)
-		m = np.identity(k) - term
+		m = 1 - term
 		p_post = np.dot(m, np.dot(m.T, p_pri)) + np.dot(kalman_gain, np.dot(self.matrix_R, kalman_gain.T))
 		return x_post, p_post
 
@@ -32,17 +31,24 @@ if __name__ == '__main__':
 
 	z = Part_2a.generate_time_series(1.5, -1, 3, 200)
 	a = np.array([[1.5, -1], [0, 0]])
-	p = np.array([[3, 0], [0, 3]])
-	q = np.array([[3, 0], [0, 0]])
+	p = np.array([[0.1, 0], [0, 0.1]])
+	q = np.array([[1.5, 0], [0, 0]])
 	h = np.array([1, 0])
-	r = np.array([[3, 0], [0, 0]])
+	r = np.array([[1.5, 0], [0, 0]])
 	kf = KalmanFilter(a, p, q, h, r)
+	x_initial = [0, 0]
+	p_initial = copy.deepcopy(p)
+	z_pred = []
 	for i in range(0, len(z)):
-		x_initial = [0, 0]
-		p_initial = copy.deepcopy(p)
 		x_in, p_in = kf.predict(x_initial, p_initial)
 		x_post, p_post = kf.update(x_in, p_in, z[i])
-
+		z_pred.append(np.dot(h, x_post))
+		x_initial = x_post
+		p_initial = p_post
+		print(z)
+	plt.plot(z, color='green')
+	plt.plot(z_pred)
+	plt.show()
 
 
 
