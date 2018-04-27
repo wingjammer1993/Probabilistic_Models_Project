376652415e9b21283a5566f2b0f3277bff381a80
amruta <hail.radical@gmail.com>
2018-04-26 22:47:33 -0600

    added noise

--- Part_2c.py
+++ Part_2c.py
@@ -13,28 +13,28 @@ class KalmanFilter:
 
 	def predict(self, x_init, p_init):
 		x_priori = np.dot(self.matrix_A, x_init)
-		p_priori = np.dot(self.matrix_A, np.dot(p_init, self.matrix_A.T))
+		p_priori = np.add(np.dot(self.matrix_A, np.dot(p_init, self.matrix_A.T)), self.matrix_Q)
 		return x_priori, p_priori
 
 	def update(self, x_pri, p_pri, observation):
 		innovation = observation - np.dot(self.matrix_H, x_pri)
 		innovation_cov = self.matrix_R + np.dot(self.matrix_H, np.dot(p_pri, self.matrix_H.T))
-		kalman_gain = np.dot(p_pri, np.dot(self.matrix_H.T, np.linalg.inv(innovation_cov)))
+		kalman_gain = np.dot(p_pri, self.matrix_H) / innovation_cov
 		x_post = x_pri + np.dot(kalman_gain, innovation)
 		term = np.dot(kalman_gain, self.matrix_H)
 		m = 1 - term
-		p_post = np.dot(m, np.dot(m.T, p_pri)) + np.dot(kalman_gain, np.dot(self.matrix_R, kalman_gain.T))
+		p_post = np.dot(m, p_pri)
 		return x_post, p_post
 
 
 if __name__ == '__main__':
 
-	z = Part_2a.generate_time_series(1.5, -1, 3, 200)
-	a = np.array([[1.5, -1], [1, 0]])  # state transition matrix
-	p = np.array([[0.9, 0], [0, 0.9]])  # process covariance
+	z = Part_2a.generate_time_series(1.5, -1, 0.2, 200)
+	a = np.array([[1.5, -1], [-1, 0]])  # state transition matrix
+	p = np.array([[0.5, 0], [0, 0.5]])  # process covariance
 	q = np.array([[5, 0], [0, 5]])  # state covariance
 	h = np.array([1, 0])  # observation matrix
-	r = np.array([[1.732, 0], [0, 1.732]])  # observation covariance
+	r = np.array([3])  # observation covariance
 	kf = KalmanFilter(a, q, h, r)
 	x_initial = [0, 0]
 	p_initial = copy.deepcopy(p)
@@ -42,6 +42,7 @@ if __name__ == '__main__':
 	for i in range(0, len(z)):
 		x_in, p_in = kf.predict(x_initial, p_initial)
 		x_postr, p_postr = kf.update(x_in, p_in, z[i])
+		h = h.reshape(1,2)
 		z_pred.append(np.dot(h, x_postr))
 		x_initial = x_postr
 		p_initial = p_postr
