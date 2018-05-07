b35456688cba707112dcdf6829f4d7bf9f733eaf
amruta <hail.radical@gmail.com>
2018-05-06 18:59:01 -0600

    added noise

--- Part_2a.py
+++ Part_2a.py
@@ -13,13 +13,15 @@ def generate_time_series(alpha_1, alpha_2, sigma, samples):
 		y_2[i] = y_1[i-1] + mu_y2
 		y_1[i] = alpha_1*y_1[i-1] + alpha_2*y_2[i-1] + mu_y1
 		z[i] = y_1[i] + mu_z
-	plt.plot(z)
+	plt.plot(z, label="z value")
+	plt.plot(y_1, color="green", label="y1 value")
 	plt.xlabel("Time")
 	plt.ylabel("Observation")
-	plt.title("Autoregressive Time Series")
+	plt.title("Autoregressive Time Series with sigma = {}, alpha1 = {}, alpha2 = {}".format(sigma, alpha_1, alpha_2))
+	plt.legend()
 	plt.show()
 	return z, y_1
 
 
-generate_time_series(1.5, -1, 3, 200)
+generate_time_series(1.5, -0.99, 0.2, 200)
 
